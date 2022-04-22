26a8ff446b594f1972294a55994e0f9b51eebced
minho park <riderkaman@gmail.com>
2022-04-22 14:38:19 +0900

    <test> 게임 인풋 테스트 추가함.
    - ApplicationTest 케이스 오류사항 수정
    - 클래스 설계 구조 재구성해야 함. 다른 방식으로 구성해 볼 것.

--- src/main/java/racingcar/GameIO.java
+++ src/main/java/racingcar/GameIO.java
@@ -3,6 +3,22 @@ package racingcar;
 public class GameIO {
 
 
+    public static boolean isCorrectCarName(String s) {
 
+        if (s == null) {
+            return false;
+        }
 
+        if (!s.contains(",")) {
+            return false;
+        }
+
+        String[] names = s.split(",");
+
+        for (int i = 0; i < names.length; i++) {
+
+        }
+
+        return true;
+    }
 }
